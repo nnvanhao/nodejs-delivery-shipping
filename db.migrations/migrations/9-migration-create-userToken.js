4139c6309060b4c976002bd372da3a50413e64bc
Hung Ngo <bapdola304@gmail.com>
2021-09-28 19:27:45 +0700

    update migration table name usertoken

--- db.migrations/migrations/9-migration-create-userToken.js
+++ db.migrations/migrations/9-migration-create-userToken.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        await queryInterface.createTable('usertoken', {
+        await queryInterface.createTable('usertokens', {
             id: {
                 allowNull: false,
                 primaryKey: true,
@@ -31,6 +31,6 @@ module.exports = {
         });
     },
     down: async (queryInterface, Sequelize) => {
-        await queryInterface.dropTable('usertoken');
+        await queryInterface.dropTable('usertokens');
     }
 };
